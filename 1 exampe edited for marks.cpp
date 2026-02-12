522019f740f15f4806aeb47f13eb963e8689bede
mharoon2886 <m.haroon2886@gmail.com>
2026-02-12 20:18:51 +0500

    Add files via upload

---
+++ 1 exampe edited for marks.cpp
@@ -0,0 +1,16 @@
+     // Fig. 1.2: fig01_02.cpp
+     // assd
+    // A first program in C++
+	 #include <iostream> // aghar ham # remove karte he to compiler hame 'include' does not name a type ka error deta he
+using namespace std;
+// function main begins program execution
+
+    int main(){ // aghar ham int main remove karte he to hamare pas unqualified-id before '{' token ka error ayega or aghar ham int main remove karte he to hamare pas entry point he nahi hoga is wajha hamara programm run nahi hoga
+    	/* this 
+		is 
+		you
+		multiline 
+		comment*/
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
