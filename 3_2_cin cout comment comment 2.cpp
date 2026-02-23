197fad17980eb4f22b8cf061447ee1eb2b2216aa
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:27:51 +0500

    Add files via upload

---
+++ 3_2_cin cout comment comment 2.cpp
@@ -0,0 +1,18 @@
+     // Fig. 1.2: fig01_02.cpp
+    // A first program in C++.
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()
+    {
+    cout << "I am in CS department."<<endl;
+    
+    cout << "I am programming enthusiast.";/* cout basically  string literals(i am programming enthusiast) ko output device jeise ke hamare monitor per 
+	print/show karta he*/  
+	/* "<<" is ko insertion ooperator kehte he ,ye cout ke help karta he , jeise ke data ko monitor per show/prinnt karne kelye data ko cout stream
+	 me beijh deta he*/
+	  
+    cout <<"i am ronaldo 7!";
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
