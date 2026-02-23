51dac38f1550d4c209d4466060c23c87472ba109
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:23:37 +0500

    Add files via upload

---
+++ 1_example 1 comments.cpp
@@ -0,0 +1,16 @@
+     // Fig. 1.2: fig01_02.cpp
+     // assd
+    // A first program in C++.
+#include <iostream> //ye code ke run/compile hone se pehle compiler ko ouut or input ke bare me  hidayaat dete he yani ye preprocessor directive ka kam karta he   
+using namespace std;// ye lekhne se bar bar std:: lekhne ke zarorat nahi rehti. for example std::cout ke jagha sirf cout lekh sakhte he
+// function main begins program execution
+int main() //  ye main funtion hay he es se parogram start hota hay
+    
+    	{/* this 
+		is 
+		you
+		multiline // ye /* multi line comment k liye istemal hota 
+		comment*/
+    return 0;   // indicate that program ended successfully // ye hama batata he ke program khatam ho chuka he
+     }// end function main
+
