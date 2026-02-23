5d3b5d2f66408d17a80e01a1441b4a674525c793
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:22:59 +0500

    Add files via upload

---
+++ 1 exampe for error.cpp
@@ -0,0 +1,27 @@
+     // Fig. 1.2: fig01_02.cpp
+     // assd
+    // A first program in C++
+	 #include <iostream> // aghar ham # remove karte he to compiler hame 'include' does not name a type ka error deta he
+	 /* aghar ham #include <iostream> nahi lekhte to compiler ko output,input ke samajh he nahi ayege for example: aghar ham cout or cin lekhege to 
+	 compler ko is ke samajh nahi ayege*/
+ 
+using namespace std;// aghar ham using namespace std ;nahi lekhege to hame bar bar cout ke sath std lekhna parega 
+// function main begins program execution
+
+   int main()
+	{ /* aghar ham int main remove karte he to hamare pas unqualified-id before '{' token ka error ayega or aghar ham int main remove karte he to hamare
+	 pas entry point he nahi hoga is wajha hamara programm run nahi hoga*/
+    	/* this 
+		is 
+		you 
+		multiline 
+		comment*/
+		cout<< "gdffjhuiyuyt";
+		cin>>"hghghkhgd";
+    return 0;   // indicate that program ended successfully
+    } // end function main
+    /*gdhjhkhkh
+    hfgfjukkhkilklo
+    */
+    
+
