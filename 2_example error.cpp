e77e38242df88a305b3114859112d5990e2a1a43
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:25:55 +0500

    Add files via upload

---
+++ 2_example error.cpp
@@ -0,0 +1,11 @@
+     // Fig. 1.2: fig01_02.cpp
+    // A first program in C++.
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()
+    {
+     cout<< "Welcome to C++!\n"; // aghar ham insertion operator (<<) ke sath cout nahi lekhge to compiler ko samajh nahi ayega ke data ko kedhar send karna he yani cout is required to send data/text to the standarad output(your console) 
+    return 0 ;   // indicate that program ended successfully
+    } // end function main
+
