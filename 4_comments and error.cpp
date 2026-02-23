8dc3b1c6d88b85878516eeeaf351d9ceb475beb7
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:28:40 +0500

    Add files via upload

---
+++ 4_comments and error.cpp
@@ -0,0 +1,15 @@
+     // Fig. 1.2: fig01_02.cpp
+    // A first program in C++.
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()  
+	
+	
+	cout << "I am in \r CS department. \n";
+ return 0;   // indicate that program ended successfully
+    /*"{}" is ko curly braces or block braces kehte he.jo cheiz is ke andar lekhe jati he wo "main()" ka hissa hote he.without these curly braces the
+    function has no body*/
+/*ERROR:
+aghar ham curly braces nahi lekhinge to compiler hame expected intializer before 'cout' and expected unqualified-id before return ka error dega   */
+
