78c524479e6ee636db1e5007abfb8a3e04ed684f
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:34:36 +0500

    Add files via upload

---
+++ lab2task1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a = 5; 
+float b = 2.16; 
+double c = 8.5423;
+
+ cout<<"value of a "<<a<<endl;
+ cout<<"size of a "<<sizeof(a)<<endl;
+ cout<<"location of a "<<&a<<endl;
+ 
+ cout<<"value of b "<<b<<endl;
+ cout<<"size of b "<<sizeof(b)<<endl;
+ cout<<"location of b "<<&b<<endl;
+ 
+ cout<<"value of c "<<c<<endl;
+ cout<<"size of c "<<sizeof(c)<<endl;
+ cout<<"location of c "<<&c<<endl;
+
+ return 0;
+}
