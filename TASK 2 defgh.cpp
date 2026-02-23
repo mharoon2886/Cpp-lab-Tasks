f09823b17c6aeb09d2e8f698b8af70e63d70dbbb
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:38:12 +0500

    Add files via upload

---
+++ TASK 2 defgh.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout <<" Dear customer the service will be not available today. sorry for inconvenience created";
+	cout <<"happy to help!..."; 
+	cout <<"Great question!...";
+	cout<< "Nice to meet you";
+	cout<<"Thanks for bringing this to our attention!";
+	cout<<"I completly undersstand why you'd wnat that";
+	
+	return 0;
+}
