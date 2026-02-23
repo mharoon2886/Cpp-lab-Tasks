5920e35fd9812b5f7f89df872531603a3ff4d104
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:35:52 +0500

    Add files via upload

---
+++ rtask 5 b,c,d.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int a = 5; 
+float b = 2.16; 
+double c = 8.5423;
+
+ cout<<"value of a "<<a<<endl;
+ cout<<"size of a "<<sizeof(a)<<endl;
+ cout<<"location of a "<<&a<<endl;
+ 
+ cout<<"value of b "<<b<<endl;
+ cout<<"size of b "<<sizeof(b)<<endl;
+ cout<<"location of b "<<&b<<endl;
+ 
+ cout<<"value of c"
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ return 0;
+}
