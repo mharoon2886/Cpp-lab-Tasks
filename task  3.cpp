43fbcccef25dd6aaea67186fdeba44e85dbc7e54
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:36:21 +0500

    Add files via upload

---
+++ task  3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float tempcel,tempfah;
+	cout<<"enter temperature in celcius :"<<endl;
+	cin>>tempcel;
+	
+	
+	tempfah=(tempcel*9/5)+32;
+	cout<<"temperature in fahrenhiet ="<<tempfah<<endl;
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
