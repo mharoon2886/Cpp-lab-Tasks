d7375ba52e45a6a093eeec61375d93d7be319c8e
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:25:22 +0500

    Add files via upload

---
+++ 2_example comments.cpp
@@ -0,0 +1,11 @@
+     // Fig. 1.2: fig01_02.cpp
+    // A first program in C++.
+#include <iostream>
+using namespace std;
+// function main begins program execution
+int main()
+    {
+     cout    << "Welcome to C++!\n"; // cout ham output kelye use karte he                                    
+    return 0;   // indicate that program ended successfully
+    } // end function main
+
