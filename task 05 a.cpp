5f7f51f9ec7c70ee74c09bfd23e8779180da03cb
mharoon2886 <m.haroon2886@gmail.com>
2026-02-24 01:39:44 +0500

    Add files via upload

---
+++ task 05 a.cpp
@@ -0,0 +1,23 @@
+//Sequential 
+//print your name 10 times 
+
+#include <iostream>
+using namespace std;
+
+int main()
+	{
+	
+	
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;
+	cout<<" M.Hroon "<<endl;	
+		
+	return 0;}
+
