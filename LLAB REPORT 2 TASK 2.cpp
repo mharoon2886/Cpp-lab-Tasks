1a4ddeadfb2eeb57ae6912e79b500f8f1aa60c4d
mharoon2886 <m.haroon2886@gmail.com>
2026-03-10 09:46:05 +0500

    Add files via upload

---
+++ LLAB REPORT 2 TASK 2.cpp
@@ -0,0 +1,14 @@
+// muhammad haroon 
+// ETEA ID 5121399
+
+
+n#include <iostream>   
+using namespace std;  
+
+int main() 
+{
+    float pi = 3.14;                  // Create a variable named 'pi' of type float and store 3.14 in it
+    cout << "Value of pi is: " << 3.14 << endl;   // Print the message and value 3.14 on the screen
+
+    return 0;    // End the program and return 0 to the operating system
+}
